9012a769cae28c7ee6b48c58f43847d42d2b2adc
unknown <tatianaosoblivaia@gmail.com>
2024-02-22 00:28:42 +0200

    booking new

---
+++ src/index.php
@@ -0,0 +1,8 @@
+<h1>Welcom to our hotel Booking</h1>
+<?php 
+    $days_till_openning = 27; // snake_case_name
+    $bonusPlaces = 10; // camelCase
+?>
+<p>Days till opening: <?php print($days_till_openning) ?></p>
+<p>Bonus places left:<?php print($bonusPlaces) ?></p>
+<p>Book with us and trave all around the worldl!!!</p>
