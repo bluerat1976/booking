3489be550e792feb47326ab3ad3973b9cd5596e8
bluerat1976 <tatianaosoblivaia@gmail.com>
2024-04-07 00:45:03 +0300

    New commit

--- src/examples/fs/reader-2.php
+++ src/examples/fs/reader-2.php
@@ -3,8 +3,9 @@
         $file = fopen("./data/client-2.json", "r");
         $client = json_decode( fread($file, 1000), true );
         //var_dump($client);
-        if($client['age']) {
+        if($client['age'] && $client['active'])  {
            $client['age'] = (int)$client['age'];
+           $client['active'] = (bool)$client['active']; 
            var_dump($client);
         }
    
--- src/examples/fs/writer-2.php
+++ src/examples/fs/writer-2.php
@@ -2,21 +2,23 @@
 
    // $client_data = $_GET;
     var_dump($_GET); // Gives all parametrs from $_GET
-
-    if(array_key_exists('name', $_GET) && array_key_exists('email', $_GET) && array_key_exists('age', $_GET)) {
+  
+    if(array_key_exists('name', $_GET) && array_key_exists('email', $_GET) && array_key_exists('age', $_GET) && array_key_exists('active', $_GET) ) {
         
         $client = [
             'name' => $_GET['name'],
             'email' => $_GET['email'],
-            'age' => ($_GET['age']),
+            'age' => $_GET['age'],
+            'active' => $_GET['active'],
         ];   
       // var_dump('<br>'.$client);
        $file = fopen("./data/client-2.json", "w");
         
         fwrite($file, json_encode($client));
         fclose($file);
+        var_dump($client); // Gives the client's parametrs only
     } else {
         print('<br><br> Some client\'s paramets are missing. Enter all client\'s paramets: name, email, and age!');
     }
 
-   
+ 
