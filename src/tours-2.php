3820cc43c66f20ada783ae79377c4ee030a83d05
bluerat1976 <tatianaosoblivaia@gmail.com>
2024-04-16 02:18:24 +0300

    new

--- src/tours-2.php
+++ src/tours-2.php
@@ -3,12 +3,29 @@
 //   $tours = json_decode(fread($file, 1000000 ), true);
 //     fclose($file);
     $tours = load('tours');
+    
+    //--------------Search by name---------------------
     if(isset($_POST['search'])) {
         $tours = array_filter($tours, function($tour) {
             return similar_text($tour['name'], $_POST['search']) >=3 || $_POST['search'] == ' ';
         });
     }
     $tours = array_values($tours);
+
+    //---------Search by min and max prices-----------------
+
+    // Обработка формы поиска
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $min_price = $_POST["min_price"];
+    $max_price = $_POST["max_price"];
+
+    // Фильтрация туров по цене
+    $tours = array_filter($tours, function($tour) use ($min_price, $max_price) {
+        $price = $tour['price']['ammount'];
+        return $price >= $min_price && $price <= $max_price;
+    });
+}
+$tours = array_values($tours);
 ?>
 
 
@@ -22,9 +39,15 @@
         <button>SEARCH</button>
     </form>
 
-
     <!-------/FILTERS------------>
 
+    <!---------------------------->
+        <form action="/" method="post" >
+            <input name="min_price" type="text" placeholder="Enter min price" value="<?= $_POST['min_price'] ?? ' ' ?>">
+            <input name="max_price" type="text" placeholder="Enter max price" value="<?= $_POST['max_price'] ?? ' ' ?>">
+            <button>Search</button>
+        </form>
+    <!-------------------------->
     <ol>
         <? for($i=0; $i<count($tours); $i++) {?>
             <li> 
