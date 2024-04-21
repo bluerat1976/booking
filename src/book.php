87fbc9551c8d829867c931b39f2a8b48f7ba4315
bluerat1976 <tatianaosoblivaia@gmail.com>
2024-04-22 01:22:08 +0300

    Tour name with function

--- src/book.php
+++ src/book.php
@@ -1,9 +1,30 @@
 <?
     $tours = load('tours');
-    $id = $_GET['id'];
+   
+    $id = $_GET['id']; // Предполагается, что id передается через GET-параметр
+
+    // Функция для фильтрации тура по id
+    function filterById($tour) {
+        global $id;
+        return $tour['id'] == $id;
+    }
+
+    $filtered_tours = array_filter($tours, 'filterById');
+
+    if (!empty($filtered_tours)) {
+        $tour = reset($filtered_tours); // Получаем первый элемент отфильтрованного массива
+
+    }
     //HW1: find the tour by ID and print its name in H2. a) array function, b) for + if
+
+
 ?>
 
+<h2>Tour name: <?= $tour['name'] ?></h2>
+<h3>Country: <?= $tour['country'] ?></h3>
+<img src="<?= $tour['image'] ?>" style="width: 260px" alt="">
+
+<p style="font-weight: 700; color: green" >Price <?= (int)$tour['price']['ammount']?> <?=$tour['price']['currency'] ?></p>
 <hr>
 <form action="/?page=order&id=<?= $id ?>" method="POST">
     <h2>You are booking the tour</h2>
